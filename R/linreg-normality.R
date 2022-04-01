731f18bbd5973d2abeb61e164b4053f59e4d0a72
Robert Lankford <robbylankford@gmail.com>
2022-04-01 16:10:18 -0400

    Initial normality tests

---
+++ R/linreg-normality.R
@@ -0,0 +1,115 @@
+# Anderson-Darling Test -------------------------------------------------------
+
+#' Run an Anderson-Darling Test
+#'
+#' A wrapper around \code{\link[nortest]{ad.test}} that standardizes the
+#' inputs and outputs.
+#'
+#' The hypotheses for this test are:
+#'
+#' * Null: Follows a Normal Distribution
+#' * Alternative: Does Not Follow a Normal Distribution
+#'
+#' @param object a model object (such as a fitted `lm` object).
+#' @param .alpha critical p-value used to determine test conclusion.
+#'
+#' @return a [tibble][tibble::tibble-package].
+#'
+#' @examples
+#' library(dplyr)
+#' library(parsnip)
+#' library(tidytest)
+#'
+#' mod_fit <- parsnip::linear_reg() %>%
+#'   set_engine("lm") %>%
+#'   fit(mpg ~ disp + wt + hp, data = mtcars)
+#'
+#' anderson_darling_test(mod_fit)
+#'
+#' @export
+anderson_darling_test <- function(object, .alpha = 0.05) {
+  UseMethod("anderson_darling_test")
+}
+
+#' @export
+anderson_darling_test.default <- function(object, ...) {
+  stop("No method for object of class ", class(object))
+}
+
+#' @export
+anderson_darling_test.lm <- function(object, .alpha = 0.05) {
+  object %>%
+    get_residuals() %>%
+    dplyr::pull(.resid) %>%
+    nortest::ad.test() %>%
+    tidy_test(
+      statistic, p.value,
+      test  = "Anderson-Darling",
+      null  = "Follows a Normal Distribution",
+      alt   = "Does Not Follow a Normal Distribution",
+      alpha = .alpha
+    )
+}
+
+#' @export
+anderson_darling_test._lm <- function(object, .alpha = 0.05) {
+  anderson_darling_test.lm(object[["fit"]], .alpha = .alpha)
+}
+
+# Shapiro-Wilk Test -----------------------------------------------------------
+
+#' Run a Shapiro-Wilk Test
+#'
+#' A wrapper around \code{\link[stats]{shapiro.test}} that standardizes the
+#' inputs and outputs.
+#'
+#' The hypotheses for this test are:
+#'
+#' * Null: Follows a Normal Distribution
+#' * Alternative: Does Not Follow a Normal Distribution
+#'
+#' @param object a model object (such as a fitted `lm` object).
+#' @param .alpha critical p-value used to determine test conclusion.
+#'
+#' @return a [tibble][tibble::tibble-package].
+#'
+#' @examples
+#' library(dplyr)
+#' library(parsnip)
+#' library(tidytest)
+#'
+#' mod_fit <- parsnip::linear_reg() %>%
+#'   set_engine("lm") %>%
+#'   fit(mpg ~ disp + wt + hp, data = mtcars)
+#'
+#' shapiro_wilk_test(mod_fit)
+#'
+#' @export
+shapiro_wilk_test <- function(object, .alpha = 0.05) {
+  UseMethod("shapiro_wilk_test")
+}
+
+#' @export
+shapiro_wilk_test.default <- function(object, ...) {
+  stop("No method for object of class ", class(object))
+}
+
+#' @export
+shapiro_wilk_test.lm <- function(object, .alpha = 0.05) {
+  object %>%
+    get_residuals() %>%
+    dplyr::pull(.resid) %>%
+    shapiro.test() %>%
+    tidy_test(
+      statistic, p.value,
+      test  = "Shapiro-Wilk",
+      null  = "Follows a Normal Distribution",
+      alt   = "Does Not Follow a Normal Distribution",
+      alpha = .alpha
+    )
+}
+
+#' @export
+shapiro_wilk_test._lm <- function(object, .alpha = 0.05) {
+  shapiro_wilk_test.lm(object[["fit"]], .alpha = .alpha)
+}
