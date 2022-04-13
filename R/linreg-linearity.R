440811482296b638d747867a0272e3145fe4db85
Robert Lankford <robbylankford@gmail.com>
2022-04-13 14:58:48 -0400

    added harvey-collier test and methods for `glm`

--- R/linreg-linearity.R
+++ R/linreg-linearity.R
@@ -43,7 +43,7 @@ ramsey_reset.lm <- function(object, power = 2:3, ..., .alpha = 0.05) {
   object %>%
     lmtest::resettest(power = power, ...) %>%
     tidy_test(
-      statistic, p.value,
+      df1, df2, statistic, p.value,
       test  = "Ramsey's RESET",
       null  = "Linear Specification is Valid",
       alt   = "Linear Specification is Not Valid",
@@ -54,4 +54,72 @@ ramsey_reset.lm <- function(object, power = 2:3, ..., .alpha = 0.05) {
 #' @export
 ramsey_reset._lm <- function(object, power = 2:3, ..., .alpha = 0.05) {
   ramsey_reset.lm(object[["fit"]], power = power, ..., .alpha = .alpha)
+}
+
+#' @export
+ramsey_reset._glm <- function(object, power = 2:3, ..., .alpha = 0.05) {
+  ramsey_reset._lm(object, power = power, ..., .alpha = .alpha)
+}
+
+
+# Harvey-Collier Test ---------------------------------------------------------
+
+#' Run a Harvey-Collier Test
+#'
+#' A wrapper around \code{\link[lmtest]{harvtest}} that standardizes the
+#' inputs and outputs.
+#'
+#' The hypotheses for this test are:
+#'
+#' * Null: True Relationship is Linear
+#' * Alternative: True Relationship is Not Linear (Convex or Concave)
+#'
+#' @param object a model object (such as a fitted `lm` object).
+#' @param ... further arguments passed to \code{\link[lmtest]{harvtest}}.
+#' @param .alpha critical p-value used to determine test conclusion.
+#'
+#' @return a [tibble][tibble::tibble-package].
+#'
+#' @examples
+#' library(dplyr)
+#' library(parsnip)
+#' library(tidytest)
+#'
+#' mod_fit <- parsnip::linear_reg() %>%
+#'   set_engine("lm") %>%
+#'   fit(mpg ~ disp + wt + hp, data = mtcars)
+#'
+#' harvey_collier_test(mod_fit)
+#'
+#' @export
+harvey_collier_test <- function(object, ..., .alpha = 0.05) {
+  UseMethod("harvey_collier_test")
+}
+
+#' @export
+harvey_collier_test.default <- function(object, ...) {
+  stop("No method for object of class ", class(object))
+}
+
+#' @export
+harvey_collier_test.lm <- function(object, ..., .alpha = 0.05) {
+  object %>%
+    lmtest::harvtest(...) %>%
+    tidy_test(
+      statistic, p.value,
+      test  = "Harvey-Collier",
+      null  = "True Relationship is Linear",
+      alt   = "True Relationship is Not Linear (Convex or Concave)",
+      alpha = .alpha
+    )
+}
+
+#' @export
+harvey_collier_test._lm <- function(object, ..., .alpha = 0.05) {
+  harvey_collier_test.lm(object[["fit"]], ..., .alpha = .alpha)
+}
+
+#' @export
+harvey_collier_test._glm <- function(object, ..., .alpha = 0.05) {
+  harvey_collier_test._lm(object, ..., .alpha = .alpha)
 }
