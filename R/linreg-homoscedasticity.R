5c4a6fd84cddd049a5f3b07ea151ccb92b8f7a1e
Robert Lankford <robbylankford@gmail.com>
2022-04-13 14:59:07 -0400

    added goldfeld-quandt test and methods for `glm`

--- R/linreg-homoscedasticity.R
+++ R/linreg-homoscedasticity.R
@@ -5,6 +5,11 @@
 #' A wrapper around \code{\link[lmtest]{bptest}} that standardizes the inputs
 #' and outputs.
 #'
+#' The hypotheses for this test are:
+#'
+#' * Null: Variances are Equal (Homoscedastic)
+#' * Alternative: Variances are Not Equal (Heteroscedastic)
+#'
 #' @param object a model object (such as a fitted `lm` object).
 #' @param ... further arguments passed to \code{\link[lmtest]{bptest}}.
 #' @param .alpha critical p-value used to determine test conclusion.
@@ -48,4 +53,80 @@ bruesch_pagan_test.lm <- function(object, ..., .alpha = 0.05) {
 #' @export
 bruesch_pagan_test._lm <- function(object, ..., .alpha = 0.05) {
   bruesch_pagan_test.lm(object[["fit"]], ..., .alpha = .alpha)
+}
+
+#' @export
+bruesch_pagan_test._glm <- function(object, ..., .alpha = 0.05) {
+  bruesch_pagan_test._lm(object, ..., .alpha = .alpha)
+}
+
+
+# Goldfeld-Quandt Test --------------------------------------------------------
+
+#' Run a Goldfeld-Quandt Test
+#'
+#' A wrapper around \code{\link[lmtest]{gqtest}} that standardizes the inputs
+#' and outputs.
+#'
+#' The hypotheses for this test are:
+#'
+#' * Null: Variances are Equal (Homoscedastic)
+#' * Alternative: Variances are Not Equal (Heteroscedastic)
+#'
+#' @inheritParams lmtest::gqtest
+#' @param object a model object (such as a fitted `lm` object).
+#' @param ... further arguments passed to \code{\link[lmtest]{gqtest}}.
+#' @param .alpha critical p-value used to determine test conclusion.
+#'
+#' @return a [tibble][tibble::tibble-package].
+#'
+#' @examples
+#' library(dplyr)
+#' library(parsnip)
+#' library(tidytest)
+#'
+#' mod_fit <- parsnip::linear_reg() %>%
+#'   set_engine("lm") %>%
+#'   fit(mpg ~ disp + wt + hp, data = mtcars)
+#'
+#' goldfeld_quandt_test(mod_fit)
+#'
+#' @export
+goldfeld_quandt_test <- function(object, alternative = "two.sided", ...,
+                                 .alpha = 0.05) {
+  UseMethod("goldfeld_quandt_test")
+}
+
+#' @export
+goldfeld_quandt_test.default <- function(object, ...) {
+  stop("No method for object of class ", class(object))
+}
+
+#' @export
+goldfeld_quandt_test.lm <- function(object, alternative = "two.sided", ...,
+                                    .alpha = 0.05) {
+  object %>%
+    lmtest::gqtest(...) %>%
+    tidy_test(
+      df1, df2, statistic, p.value,
+      test  = "Goldfeld-Quandt",
+      null  = "Variances Are Equal",
+      alt   = "Variances Are Not Equal",
+      alpha = .alpha
+    )
+}
+
+#' @export
+goldfeld_quandt_test._lm <- function(object, alternative = "two.sided", ...,
+                                     .alpha = 0.05) {
+  goldfeld_quandt_test.lm(
+    object[["fit"]], alternative = alternative, ..., .alpha = .alpha
+  )
+}
+
+#' @export
+goldfeld_quandt_test._glm <- function(object, ..., .alpha = 0.05) {
+  goldfeld_quandt_test._lm(
+    object, alternative = alternative, ..., .alpha = .alpha
+  )
 }
