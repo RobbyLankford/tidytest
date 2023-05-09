cf46904565162d1bbc225ce8dcf11bcdac4b9633
Lankford <Robert.Lankford@Ally.com>
2023-05-09 15:47:03 -0400

    remove default S3 method and use roxygen2 templates for normality tests

--- R/linreg-normality.R
+++ R/linreg-normality.R
@@ -2,58 +2,29 @@
 
 #' Run an Anderson-Darling Test
 #'
-#' A wrapper around \code{\link[nortest]{ad.test}} that standardizes the
-#' inputs and outputs.
-#'
+#' @details
 #' The hypotheses for this test are:
 #'
 #' * Null: Follows a Normal Distribution
 #' * Alternative: Does Not Follow a Normal Distribution
 #'
-#' @inheritParams bruesch_pagan_test
-#' @inheritParams ljung_box_test
+#' @templateVar link nortest::ad.test
+#' @template desc-linreg-tests
+#'
+#' @family normality tests
+#' @template return
+#'
+#' @template params-linreg
+#' @template params-linreg-vec
 #' @param ... Not currently used.
 #'
-#' @return A [tibble][tibble::tibble-package].
-#'
-#' @examples
-#' library(dplyr)
-#' library(parsnip)
-#' library(tidytest)
-#'
-#' #> Numeric Method
-#' set.seed(1914)
-#' resids <- rnorm(n = 100)
-#'
-#' anderson_darling_test(resids)
-#'
-#' #> `lm` Method
-#' mod_lm_fit <- lm(mpg ~ disp + wt + hp, data = mtcars)
-#'
-#' anderson_darling_test(mod_lm_fit)
-#'
-#' #> Tidymodels Method
-#' mod_linreg_fit <- parsnip::linear_reg() %>%
-#'   set_engine("lm") %>%
-#'   fit(mpg ~ disp + wt + hp, data = mtcars)
-#'
-#' anderson_darling_test(mod_linreg_fit)
+#' @templateVar fn anderson_darling_test
+#' @template examples-linreg-tests
+#' @template examples-linreg-tests-vec
 #'
 #' @export
 anderson_darling_test <- function(object, ..., .alpha = 0.05) {
   UseMethod("anderson_darling_test")
-}
-
-#' @rdname anderson_darling_test
-#' @export
-anderson_darling_test.default <- function(object, ...) {
-  stop("No method for object of class ", class(object))
-}
-
-#' @rdname anderson_darling_test
-#' @export
-anderson_darling_test.numeric <- function(x, ..., .alpha = 0.05) {
-  anderson_darling_test_spec(x, ..., .alpha = .alpha)
 }
 
 #' @rdname anderson_darling_test
@@ -80,60 +51,40 @@ anderson_darling_test._glm <- function(object, ..., .alpha = 0.05) {
   anderson_darling_test_spec(resids, ..., .alpha = .alpha)
 }
 
+#' @rdname anderson_darling_test
+#' @export
+anderson_darling_test_vec <- function(x, ..., .alpha = 0.05) {
+  anderson_darling_test_spec(x, ..., .alpha = .alpha)
+}
+
+
 # Shapiro-Wilk Test -----------------------------------------------------------
 
 #' Run a Shapiro-Wilk Test
 #'
-#' A wrapper around \code{\link[stats]{shapiro.test}} that standardizes the
-#' inputs and outputs.
-#'
+#' @details
 #' The hypotheses for this test are:
 #'
 #' * Null: Follows a Normal Distribution
 #' * Alternative: Does Not Follow a Normal Distribution
 #'
-#' @inheritParams anderson_darling_test
+#' @templateVar link stats::shapiro.test
+#' @template desc-linreg-tests
 #'
-#' @return A [tibble][tibble::tibble-package].
+#' @family normality tests
+#' @template return
 #'
-#' @examples
-#' library(dplyr)
-#' library(parsnip)
-#' library(tidytest)
+#' @template params-linreg
+#' @template params-linreg-vec
+#' @param ... Not currently used.
 #'
-#' #> Numeric Method
-#' set.seed(1914)
-#' resids <- rnorm(n = 100)
-#'
-#' shapiro_wilk_test(resids)
-#'
-#' #> `lm` Method
-#' mod_lm_fit <- lm(mpg ~ disp + wt + hp, data = mtcars)
-#'
-#' shapiro_wilk_test(mod_lm_fit)
-#'
-#' #> Tidymodels Method
-#' mod_linreg_fit <- parsnip::linear_reg() %>%
-#'   set_engine("lm") %>%
-#'   fit(mpg ~ disp + wt + hp, data = mtcars)
-#'
-#' shapiro_wilk_test(mod_linreg_fit)
+#' @templateVar fn shapiro_wilk_test
+#' @template examples-linreg-tests
+#' @template examples-linreg-tests-vec
 #'
 #' @export
 shapiro_wilk_test <- function(object, ..., .alpha = 0.05) {
   UseMethod("shapiro_wilk_test")
-}
-
-#' @rdname shapiro_wilk_test
-#' @export
-shapiro_wilk_test.default <- function(object, ...) {
-  stop("No method for object of class ", class(object))
-}
-
-#' @rdname shapiro_wilk_test
-#' @export
-shapiro_wilk_test.numeric <- function(x, ..., .alpha = 0.05) {
-  shapiro_wilk_test_spec(x, ..., .alpha = .alpha)
 }
 
 #' @rdname shapiro_wilk_test
@@ -160,6 +111,12 @@ shapiro_wilk_test._glm <- function(object, ..., .alpha = 0.05) {
   shapiro_wilk_test_spec(resids, ..., .alpha = 0.05)
 }
 
+#' @rdname shapiro_wilk_test
+#' @export
+shapiro_wilk_test_vec <- function(x, ..., .alpha = 0.05) {
+  shapiro_wilk_test_spec(x, ..., .alpha = .alpha)
+}
+
 
 # Helper Functions ------------------------------------------------------------
 anderson_darling_test_spec <- function(resids, ..., .alpha = 0.05) {
@@ -167,9 +124,9 @@ anderson_darling_test_spec <- function(resids, ..., .alpha = 0.05) {
     resids,
     nortest::ad.test,
     ...,
-    .test   = "Anderson-Darling",
-    .null   = "Follows a Normal Distribution",
-    .alt    = "Does Not Follow a Normal Distribution",
+    .test  = "Anderson-Darling",
+    .null  = "Follows a Normal Distribution",
+    .alt   = "Does Not Follow a Normal Distribution",
     .alpha = .alpha
   )
 }
@@ -179,9 +136,9 @@ shapiro_wilk_test_spec <- function(resids, ..., .alpha = 0.05) {
     resids,
     shapiro.test,
     ...,
-    .test   = "Shapiro-Wilk",
-    .null   = "Follows a Normal Distribution",
-    .alt    = "Does Not Follow a Normal Distribution",
+    .test  = "Shapiro-Wilk",
+    .null  = "Follows a Normal Distribution",
+    .alt   = "Does Not Follow a Normal Distribution",
     .alpha = .alpha
   )
 }
